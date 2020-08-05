4221a8b6f861993e9e66b5785a833ba652d5fa75
ashwin19971 <ashwingiri1997@gmail.com>
2020-08-06 01:04:15 +0530

    Updates

---
+++ precache-manifest.c485760486089116df58aad76027fc57.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d234a5a87a18f270d41f9033ce6143",
+    "url": "/spacex-missions/index.html"
+  },
+  {
+    "revision": "46d9a4d12d39f438a2b9",
+    "url": "/spacex-missions/static/css/main.076a1d5e.chunk.css"
+  },
+  {
+    "revision": "6cc2fa6ffc97591d01e2",
+    "url": "/spacex-missions/static/js/2.275602c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spacex-missions/static/js/2.275602c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d9a4d12d39f438a2b9",
+    "url": "/spacex-missions/static/js/main.70cf5eed.chunk.js"
+  },
+  {
+    "revision": "a04cab2024e22f90cf61",
+    "url": "/spacex-missions/static/js/runtime-main.42172ad3.js"
+  },
+  {
+    "revision": "11c2cd068a4bb8c39a638bb2f56eb0b1",
+    "url": "/spacex-missions/static/media/fontello.11c2cd06.woff2"
+  },
+  {
+    "revision": "9975fb69b9f2e33b1099b5968a6916a1",
+    "url": "/spacex-missions/static/media/fontello.9975fb69.eot"
+  },
+  {
+    "revision": "a108f659bcc06170b618c1be53213973",
+    "url": "/spacex-missions/static/media/fontello.a108f659.woff"
+  },
+  {
+    "revision": "e08f4fdc5cbdc1b98f29fd06e6afd7ba",
+    "url": "/spacex-missions/static/media/fontello.e08f4fdc.ttf"
+  },
+  {
+    "revision": "ef01032c1407dec1cbc19baa16617443",
+    "url": "/spacex-missions/static/media/fontello.ef01032c.svg"
+  }
+]);
